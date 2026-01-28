a28c4750b79ab38bd60827c71e78143f7fb4fc4a
Minh Le Quang <>
2026-01-28 19:39:19 +0700

    update email, phone

--- lib/me/models/request_update_phone_check.dart
+++ lib/me/models/request_update_phone_check.dart
@@ -4,12 +4,12 @@ class RequestUpdatePhoneCheck {
   RequestUpdatePhoneCheck({this.phoneNumber});
 
   RequestUpdatePhoneCheck.fromJson(Map<String, dynamic> json) {
-    phoneNumber = json["phone_number"];
+    phoneNumber = json["new_phone_number"];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data["phone_number"] = phoneNumber;
+    data["new_phone_number"] = phoneNumber;
     return data;
   }
 }
